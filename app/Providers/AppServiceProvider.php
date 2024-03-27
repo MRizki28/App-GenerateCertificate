61843b1e4c2a8d2ff1a9b4d2d4eba9c6e588b53a
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-03-27 17:33:48 +0800

    create service template

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\TemplateCertificateInterfaces;
+use App\Repositories\TemplateCertificateRepositories;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TemplateCertificateInterfaces::class, TemplateCertificateRepositories::class);
     }
 
     /**
