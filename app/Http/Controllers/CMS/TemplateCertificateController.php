9c0a58e19d5d3f41ac703bbf892d43efc724b5f8
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-03-27 17:33:23 +0800

    create service template

---
+++ app/Http/Controllers/CMS/TemplateCertificateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TemplateCertificateController extends Controller
+{
+    //
+}
